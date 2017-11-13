280b5f4c05029060b919c821bbcbbe13b76cdb79
李启文 <selfassu@gmail.com>
2017-11-13 14:34:55 +0800

    two sum

---
+++ notes-code/src/com/lqwit/algorithm/leetcode/Test01_TwoSum.java
@@ -0,0 +1,30 @@
+package com.lqwit.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test01_TwoSum {
+
+    public static void main(String[] args){
+        int nums[] =new int[]{2, 7, 11, 15};
+        int target = 9;
+        int[] ints = twoSum(nums, target);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i]);
+        }
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i + 1;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i + 1);
+        }
+        return result;
+    }
+}
